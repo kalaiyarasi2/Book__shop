208a7f9a9a7759603d7bb0d759fc6b6ffb0841e3
kalaiyarasi2 <144701600+kalaiyarasi2@users.noreply.github.com>
2024-05-29 21:53:16 +0530

    modify added

--- book_shop/books/urls.py
+++ book_shop/books/urls.py
@@ -26,16 +26,16 @@ from .views import BookListCreate, BookRetrieveUpdateDestroy, AuthorListCreate, 
 
 urlpatterns = [
     # Book URLs
-    path('books/', BookListCreate.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book-retrieve-update-destroy'),
+    path('books/', BookListCreate.as_view(), name='book_list_create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book_retrieve_update_destroy'),
 
     # Author URLs
-    path('authors/', AuthorListCreate.as_view(), name='author-list-create'),
-    path('authors/<int:pk>/', AuthorRetrieveUpdateDestroy.as_view(), name='author-retrieve-update-destroy'),
+    path('authors/', AuthorListCreate.as_view(), name='author_list_create'),
+    path('authors/<int:pk>/', AuthorRetrieveUpdateDestroy.as_view(), name='author_retrieve_update_destroy'),
 
     # Genre URLs
-    path('genres/', GenreListCreate.as_view(), name='genre-list-create'),
-    path('genres/<int:pk>/', GenreRetrieveUpdateDestroy.as_view(), name='genre-retrieve-update-destroy'),
+    path('genres/', GenreListCreate.as_view(), name='genre_list_create'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroy.as_view(), name='genre_retrieve_update_destroy'),
 ]
 from django.contrib import admin
 from django.urls import path, include
